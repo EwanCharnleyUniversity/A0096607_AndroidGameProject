d881aa219f374382fc44bb2309aaae3e2603c172
Ewan Charnley <A0096607@live.tees.ac.uk>
2024-10-18 16:59:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a0096607_androidgameproject/GameView.java
@@ -0,0 +1,16 @@
+package com.example.a0096607_androidgameproject;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ app/src/main/java/com/example/a0096607_androidgameproject/MainActivity.java
@@ -0,0 +1,28 @@
+
+package com.example.a0096607_androidgameproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
